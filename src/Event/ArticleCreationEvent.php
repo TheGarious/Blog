923d258e1ae707cac501452dd7ba7913541d069d
gary houbre <ghoubre@sqli.com>
2019-03-06 08:24:58 +0100

    Add Event when article created, we send email for user newsletter

---
+++ src/Event/ArticleCreationEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Event;
+
+use App\Entity\Article;
+use Symfony\Component\EventDispatcher\Event;
+
+class ArticleCreationEvent extends Event
+{
+    const EVENT_NAME = 'article.creation_event';
+
+    protected $article;
+
+    public function __construct(Article $article = null)
+    {
+        $this->article = $article;
+    }
+
+    /**
+     * @return Article
+     */
+    public function getArticle()
+    {
+        return $this->article;
+    }
+
+    /**
+     * @param Article $article
+     */
+    public function setArticle(Article $article)
+    {
+        $this->article = $article;
+    }
+}
---
+++ src/EventListener/ArticleSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use AppBundle\Event\ArticleCreationEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ArticleSubscriber implements EventSubscriberInterface
+{
+    public function __construct()
+    {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ArticleCreationEvent::EVENT_NAME => 'onCreation',
+        ];
+    }
+
+    public function onCreation(ArticleCreationEvent $event)
+    {
+        // TODO need created a newsletter for use it
+    }
+}
