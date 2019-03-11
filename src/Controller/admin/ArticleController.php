fbadf0cef82b9ef32b6539f12139c0f939427356
gary houbre <ghoubre@sqli.com>
2019-03-11 19:13:08 +0100

    Add event in new submit

--- src/Controller/admin/ArticleController.php
+++ src/Controller/admin/ArticleController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\admin;
 
 use App\Entity\Article;
+use App\Event\ArticleCreationEvent;
 use App\Form\ArticleType;
 use App\Manager\ArticleManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -45,7 +47,7 @@ class ArticleController extends AbstractController
      * @param Request $request
      * @return RedirectResponse|Response
      */
-    public function new(Request $request)
+    public function new(Request $request, EventDispatcherInterface $eventDispatcher)
     {
         $article = new Article();
 
@@ -55,6 +57,9 @@ class ArticleController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $article->setUser($this->getUser());
             $this->articleManager->update($article);
+
+            // Event
+            $eventDispatcher->dispatch(ArticleCreationEvent::EVENT_NAME, new ArticleCreationEvent($article));
 
             return $this->redirectToRoute("dashboard_article_index");
         }
